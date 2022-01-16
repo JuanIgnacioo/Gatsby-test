321a6f7a533aa771d9cdf8b600b1aacfe5e3b945
Juan_Ignacio_C <juanignacio.c@enviopack.com>
2022-01-16 18:32:03 -0300

    Centrado descripcion imagenes

--- src/components/Gallery/components/GalleryItem.js
+++ src/components/Gallery/components/GalleryItem.js
@@ -1,26 +1,40 @@
 import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
 
-const GalleryItem = ({id, source, thumbnail, caption, description, position, toggleLightbox}) => {
+const GalleryItem = ({
+  id,
+  source,
+  thumbnail,
+  caption,
+  description,
+  position,
+  toggleLightbox,
+}) => {
+  const onClick = useCallback(
+    (e) => {
+      e.preventDefault()
+      toggleLightbox(position)
+    },
+    [position, toggleLightbox]
+  )
 
-    const onClick = useCallback((e) => {
-        e.preventDefault()
-        toggleLightbox(position)
-    }, [position, toggleLightbox]);
+  const styleDescription = {
+    textAlign: 'center',
+  }
 
-    return (<article key={id} className="6u 12u$(xsmall) work-item">
-        <a
-        className="image fit thumb"
-        href={source}
-        onClick={onClick}
-        >
+  return (
+    <article key={id} className="6u 12u$(xsmall) work-item">
+      <a className="image fit thumb" href={source} onClick={onClick}>
         <img src={thumbnail} />
-        </a>
-
+      </a>
+      <div style={styleDescription}>
         <h3>{caption}</h3>
-        {/* <p>{description}</p> */}
-    </article>)
-};
+      </div>
+
+      {/* <p>{description}</p> */}
+    </article>
+  )
+}
 
 GalleryItem.displayName = 'GalleryItem'
 GalleryItem.propTypes = {
@@ -30,7 +44,7 @@ GalleryItem.propTypes = {
   caption: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   position: PropTypes.number.isRequired,
-  toggleLightbox: PropTypes.func.isRequired
+  toggleLightbox: PropTypes.func.isRequired,
 }
 
 export default GalleryItem
